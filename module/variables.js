f5ec62fd8f4b708264b2e9d72feffd65da7f3546
rubigit <rcortes1708.rcj@gmail.com>
2021-08-20 23:46:43 -0400

    Add variables

--- module/variables.js
+++ module/variables.js
@@ -1,8 +1,8 @@
 
 const reasonDescription = [
     "Talk to us about your preferred working method. We try to accommodate our clients to the best of our abilities.",
-    "Websites can easily cost tens of thousands of dollars, which is just not plausible for small businesses. I create cost-effective designs and am transparent about pricing up front. There are no hidden costs or monthly fees. ",
-    "Unlike some designers who focus solely on the artistic elements of a website and ignore the user experience, my websites are designed with your customer in mind. Therefore, all of my sites are clean, modern and easy to navigate."
+    "Websites can easily cost tens of thousands of dollars, which is just not plausible for small businesses. We create cost-effective designs and we are transparent about pricing up front. There are no hidden costs or monthly fees. ",
+    "Unlike some designers who focus solely on the artistic elements of a website and ignore the user experience, our websites are designed with your customer in mind. Therefore, all of our sites are clean, modern and easy to navigate."
 ]
 
 const reasonInf = [
@@ -30,8 +30,8 @@ const reasonInf = [
 const servDetail = [
 
     "Our approach to design reflects our commitment to best practice web design principles, user experience research and material design standards. We use this foundation to explore concepts and develop unique solutions based on the objectives of every project we take on. All of our design work is uniquely tailored to visually represent you and your brand to take you from web presence to web excellence.",
-    "Agency specializing in building secure, scalable, custom, accessible and dynamic websites. When you work with Inorbital, you work with an experienced and talented team that’s committed to helping you achieve your project goals today and make your website future-ready for tomorrow.",
-    "sEcom Service Detail On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De",
+    "Agency specializing in building secure, scalable, custom, accessible and dynamic websites. When you work with us, you work with an experienced and talented team that’s committed to helping you achieve your project goals today and make your website future-ready for tomorrow.",
+    "We build a thoughtful web design for your ecommerce to maximize your reach and profitability for your business. We ensure that your website remains market competitive, functional, customizable, and swift according to the changing needs of the ecommerce business type.",
     "Digital marketing is the best medium to connect with your clients and generate trus with your audience. We have got what you need to create multichannel campaigns to make your brand's message travel faster to reach your audience where they are",
     "Our website maintenance and support plans give you a peace of mind in knowing that your website is going to be running smoothly. Our pricing is competitive and the services that are covered under our plans are extensive."
 
